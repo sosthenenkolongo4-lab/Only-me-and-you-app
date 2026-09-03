1d03a3c4199e0d8322d07b3677b425a6dfd9ffb9
sosthenenkolongo4-lab <sosthenenkolongo4@gmail.com>
2026-09-03 22:14:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
-// ⚠️ Remplace "NOM-DU-REPO" par le nom EXACT de ton dépôt GitHub
+// Remplace "NOM-DU-REPO" par le nom EXACT de ton dépôt GitHub
 // (visible dans l'URL, ex: github.com/ton-compte/NOM-DU-REPO).
 // Sans ça, les fichiers CSS/JS ne se chargeront pas sur GitHub Pages.
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11"]
+    })
+  ],
   base: "/Only-me-and-you-app/",
 });
